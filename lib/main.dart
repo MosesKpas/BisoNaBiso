ff336fdf70442ee4b5dfafc4168adf84d31bed4e
MosesKaps <108306789+MosesKpas@users.noreply.github.com>
2024-06-08 11:23:08 +0100

    Update main.dart
    Notification à revoir

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   //Notification
-  await FirebaseApi.initNotifications();
+  //await FirebaseApi.initNotifications();
   runApp(const MyApp());
 }
 
